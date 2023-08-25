a0d4af41908baeff877b1755763a98a1aecc482e
Diego Cruz <dfcm950@gmail.com>
2023-08-25 13:31:55 -0600

    Creacion clases consola y juego

---
+++ src/lab6p2_diegocruz/Consola.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab6p2_diegocruz;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author dfcm9
+ */
+public class Consola {
+    
+    private String id, fab, modelo;
+    private int aniosuso;
+    private double precio;
+    private ArrayList<Juego> juegos = new ArrayList();
+
+    public Consola(String id, String fab, String modelo, int aniosuso, double precio) {
+        this.id = id;
+        this.fab = fab;
+        this.modelo = modelo;
+        this.aniosuso = aniosuso;
+        this.precio = precio;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFab() {
+        return fab;
+    }
+
+    public void setFab(String fab) {
+        this.fab = fab;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAniosuso() {
+        return aniosuso;
+    }
+
+    public void setAniosuso(int aniosuso) {
+        this.aniosuso = aniosuso;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public ArrayList<Juego> getJuegos() {
+        return juegos;
+    }
+
+    public void setJuegos(ArrayList<Juego> juegos) {
+        this.juegos = juegos;
+    }
+
+    @Override
+    public String toString() {
+        return "Consola{" + "id=" + id + ", fab=" + fab + ", modelo=" + modelo + ", aniosuso=" + aniosuso + ", precio=" + precio + ", juegos=" + juegos + '}';
+    }
+    
+    
+    
+    
+    
+}
---
+++ src/lab6p2_diegocruz/Juego.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab6p2_diegocruz;
+
+import java.util.Date;
+
+/**
+ *
+ * @author dfcm9
+ */
+public class Juego {
+    
+    String nom, desc;
+    Date fechalan;
+    double precio;
+    boolean nuevo, rentable, agregado;
+    int cant;
+
+    public Juego(String nom, String desc, Date fechalan, double precio, boolean nuevo, boolean rentable, boolean agregado, int cant) {
+        this.nom = nom;
+        this.desc = desc;
+        this.fechalan = fechalan;
+        this.precio = precio;
+        this.nuevo = nuevo;
+        this.rentable = rentable;
+        this.agregado = agregado;
+        this.cant = cant;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Date getFechalan() {
+        return fechalan;
+    }
+
+    public void setFechalan(Date fechalan) {
+        this.fechalan = fechalan;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public boolean isNuevo() {
+        return nuevo;
+    }
+
+    public void setNuevo(boolean nuevo) {
+        this.nuevo = nuevo;
+    }
+
+    public boolean isRentable() {
+        return rentable;
+    }
+
+    public void setRentable(boolean rentable) {
+        this.rentable = rentable;
+    }
+
+    public boolean isAgregado() {
+        return agregado;
+    }
+
+    public void setAgregado(boolean agregado) {
+        this.agregado = agregado;
+    }
+
+    public int getCant() {
+        return cant;
+    }
+
+    public void setCant(int cant) {
+        this.cant = cant;
+    }
+
+    @Override
+    public String toString() {
+        return "Juego{" + "nom=" + nom + ", desc=" + desc + ", fechalan=" + fechalan + ", precio=" + precio + ", nuevo=" + nuevo + ", rentable=" + rentable + ", agregado=" + agregado + ", cant=" + cant + '}';
+    }
+    
+    
+    
+    
+}
